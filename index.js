25591390464c93680a21db3a78876c37f9feb2d5
Bilal Ahmad <98654652+bilalahmad3013@users.noreply.github.com>
2023-08-08 11:25:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const mongoDB = require('./db');
 mongoDB();
 
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'https://sensegrass-front.onrender.com');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
